0a521fd8fac19a33d266cfafda2071a62b5ca542
everyany <everyany5000@gmail.com>
2021-10-29 23:45:53 -0400

    updated update query it still doesn't work though

--- project/quiz1.php
+++ project/quiz1.php
@@ -197,14 +197,10 @@ button
     $error = " ";
 
 	//the problem area
-    /*
-        $query = "UPDATE gradebook SET q1Done = ?, qlg = ?, progress = ? WHERE userID = ?";
-		$stmt = $conn->prepare($query);
-		$stmt -> bind_param("idd", $q1Done, $q1g, $progress);
-		$stmt -> execute();
+        $query = "UPDATE gradebook SET q1Done = '$q1Done', qlg = '$q1g', progress = '$progress' WHERE userID = '$userID'";
+		mysqli_query($conn, $query);
 
 		header("Location: gradebook.php");
         die;
-	*/
   }
 ?>
