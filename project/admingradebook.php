4822891d993f1aada52898ddba5dbb18edf4e2cb
everyany <everyany5000@gmail.com>
2021-11-14 01:52:24 -0500

    Update admingradebook.php

--- project/admingradebook.php
+++ project/admingradebook.php
@@ -1,11 +1,37 @@
+<?php
+  session_start();
+  include("dbconnection.php");
+  $SQL2 = "SELECT * FROM users WHERE uname = '".$_SESSION['uname']."'";//select statement variable that stores conditions for a query
+   $res2 = mysqli_query($conn, $SQL2);//plugs select statement variable into a mysqli query specifying the conn database as the source
+
+while($row2 = mysqli_fetch_array($res2)) {//while a row/rows carrying the proper conditions applied in the select statement exists $row will be equal to an array of those row values
+     $userID =  $row2['userID'] . "<br />";//setting a variable equal to a specific row in the array by specifying it's name
+   }
+
+   $SQL2 = "SELECT * FROM gradebook WHERE userID = '$userID'";
+
+   $res2 = mysqli_query($conn, $SQL2);
+
+   while($row2 = mysqli_fetch_array($res2)) {
+
+     $q1g =  $row2['q1g'] . "<br />";
+	 $q2g = $row2['q2g'] . "<br />";
+	 $q3g = $row2['q3g'] . "<br />";
+	 $progress = $row2['progress'] . "<br />";
+   }
+
+   $avgGrade = (($q1g + $q2g + $q3g)/3/5)*100;
+   $query = "UPDATE `gradebook` SET `avgGrade`='$avgGrade' WHERE `userID` = '$userID'";
+   mysqli_query($conn, $query);
+?>
+
 <!DOCTYPE html>
 <html>
-<body>
 <style>
-table
-{
+
+div.contents{
 	background-color: CornflowerBlue;
-	width: 800px;
+	width: 1000px;
 	height: 600px;
 	position: absolute;
 	text-align: center;
@@ -13,6 +39,32 @@ table
 	left: 50%;
 	margin-right: -50%;
 	transform: translate(-50%, -50%);
+}
+
+table
+{
+	width: 600px;
+	height: 100px;
+	position: absolute;
+	text-align: center;
+	top: 60%;
+	left: 50%;
+	margin-right: -50%;
+	transform: translate(-50%, -50%);
+	border: 1px solid black;
+}
+
+th
+{
+	font-size: 2em;
+	padding: 7px 20px;
+	border: 1px solid black;
+}
+
+td
+{
+	font-size: 1.5em;
+	border: 1px solid black;
 }
 
 body
@@ -29,57 +81,77 @@ form
 	transform: translate(-50%, -50%);
 }
 
+ul {
+	width: 750px;
+	height: 50px;
+    position: absolute;
+	text-align: center;
+	top: 25%;
+	left: 50%;
+	margin-right: -50%;
+	transform: translate(-50%, -50%);
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: white;
+}
 
-a
-{
+li {
+  float: left;
+  position: relative;
+  text-align: center;
+  top: 20%;
+  margin-left: 12%;
+}
+
+li a {
+  display: block;
+  color: blue;
+  text-align: center;
+  padding: 14px 25px;
+  font-size: 20px;
+  text-decoration: none;
+}
+li a:hover:not(.active) {
+  background-color: grey;
+}
+div.a {
+  background-color: Bisque;
+	width: 100px;
+	height: 25px;
 	position: absolute;
+	text-align: center;
 	top: 95%;
 	left: 50%;
 	margin-right: -50%;
-	transform: translate(-50%, -75%);
+	transform: translate(-50%, -50%);
 }
 </style>
-<?php
-  include("dbconnection.php");
-  $SQL2 = "SELECT * FROM users WHERE uname != '".$_SESSION['uname']."'";//select statement variable that stores conditions for a query
-   $res2 = mysqli_query($conn, $SQL2);//plugs select statement variable into a mysqli query specifying the conn database as the source
-
-while($row2 = mysqli_fetch_array($res2)) {//while a row/rows carrying the proper conditions applied in the select statement exists $row will be equal to an array of those row values
-     $userID =  $row2['userID'] . "<br />";//setting a variable equal to a specific row in the array by specifying it's name
-   }
-   $query2 = "INSERT INTO agradebook (userID) values ('$userID')";
-   mysqli_query($conn, $query2);
-
-
-echo "<table>
+<body>
+<div class=contents>
+<ul>
+ <li><a href="profile.php">Profile</a></li>
+    <li><a href="quizselection.php">Quizes</a></li>
+    <li><a href="study.php">Study</a></li>
+</ul>
+<table>
  <tr>
-  <th>User Page</th>
-  <th>First</th>
-  <th>Last</th>
+  <th>Quiz 1</th>
+  <th>Quiz 2</th>
+  <th>Quiz 3</th>
   <th>Progress</th>
   <th>Average</th>
- </tr>";
- while($row2 = mysql_fetch_array($query3))
- {echo '<tr>
- <td><a href="gradebook.php>' . $row2['userid'] . '</td>
- <td>' . $row2['uname'] . '</td>
- <td>' . $row2['lname'] . '</td>
- <td>' . $row2['progress'] . '</td>
- </tr>';
-}
-echo "</table>";
-mysqli_close($con);
+ </tr>
 
-?>
+<tr>
+ <td><?php echo $q1g; ?></td>
+ <td><?php echo $q2g; ?></td>
+ <td><?php echo $q3g; ?></td>
+ <td><?php echo $progress; ?></td>
+ <td><?php echo $avgGrade; ?></td>
+</tr>
+</table>
+</div>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
